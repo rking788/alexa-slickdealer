d68f289dcd2a6cf80a6a019e8ecedacb985287b2
Rob King <rking188@gmail.com>
2018-12-02 12:06:25 -0500

    Add custom intent handlers for each named intent

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/mikeflynn/go-alexa/skillserver"
+)
+
+const (
+	// Built-in intents
+	helpIntent   = "AMAZON.HelpIntent"
+	cancelIntent = "AMAZON.CancelIntent"
+	stopIntent   = "AMAZON.StopIntent"
+
+	// Custom Intents
+	frontpageDealIntent = "FrontpageDealIntent"
+	popularDealIntent   = "PopularDealIntent"
+	aboutIntent         = "AboutIntent"
 )
 
 var (
@@ -36,5 +47,58 @@ func sessionEndedHandler(request *skillserver.EchoRequest, echoResponse *skillse
 }
 
 func intentHandler(request *skillserver.EchoRequest, echoResponse *skillserver.EchoResponse) {
-	echoResponse.OutputSpeech(fmt.Sprintf("You have invoked the %s intent.", request.GetIntentName()))
+
+	var response *skillserver.EchoResponse
+
+	switch request.GetIntentName() {
+	case frontpageDealIntent:
+		response = handleFrontPageDealIntent()
+	case popularDealIntent:
+		response = handlePopularDealIntent()
+	case helpIntent:
+		response = handleHelpIntent()
+	case aboutIntent:
+		fallthrough
+	default:
+		response = handleAboutIntent()
+	}
+
+	if response == nil {
+		response = skillserver.NewEchoResponse()
+		response.OutputSpeech("Sorry, something went wrong loading the deals. Please try again later.")
+	}
+
+	*echoResponse = *response
+}
+
+func handleFrontPageDealIntent() *skillserver.EchoResponse {
+	response := skillserver.NewEchoResponse()
+	response.OutputSpeech("Frontpage deal data here")
+	response.SimpleCard("Frontpage Deals", "Frontpage deal data here")
+
+	return response
+}
+
+func handlePopularDealIntent() *skillserver.EchoResponse {
+	response := skillserver.NewEchoResponse()
+	response.OutputSpeech("Popular deal data here")
+	response.SimpleCard("Popular Deals", "Popular deal data here")
+
+	return response
+}
+
+func handleAboutIntent() *skillserver.EchoResponse {
+	response := skillserver.NewEchoResponse()
+	response.OutputSpeech("Slick Dealer was created by Rob in New Hampshire as an unofficial Slick Deals application.")
+	response.SimpleCard("About", "Slick Dealer was created by Rob in New Hampshire as an unofficial Slick Deals application.")
+
+	return response
+}
+
+func handleHelpIntent() *skillserver.EchoResponse {
+	response := skillserver.NewEchoResponse()
+	response.OutputSpeech("Help regarding the available commands here.")
+	response.SimpleCard("Frontpage Deals", "Help regarding the available commands here.")
+
+	return response
 }
